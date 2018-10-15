54ec1a0d6349fb3067e082f0d67fac75658b09ae
Rudy Ravelin <rudy@noukod.com>
2018-10-15 22:25:41 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,18 +3,18 @@ require_relative './class_methods_module.rb'
 
 class Kid
   include Dance
- 
+
   attr_accessor :name
- 
+
   def initialize(name)
     @name = name
   end
 end
 class Dancer
   include Dance
- 
+
   attr_accessor :name
- 
+
   def initialize(name)
     @name = name
   end
